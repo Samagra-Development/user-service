e49eac3dbf4f1323475a9be46bc04b6224ceb9fc
chaks03 <chakshu@samagragovernance.in>
2021-08-07 13:37:49 +0530

    Fix: Added Prisma client and schema

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +8,11 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  getHello2(): string {
     return this.appService.getHello();
   }
 }
