5d20029cd288cffb6f472b579c99f466e9d5ac4a
ChakshuGautam <chaks.gautam@gmail.com>
2021-08-11 00:20:10 +0530

    Refactor: parseResponse part of gupshup service

--- src/user/sms/gupshup/gupshup.service.ts
+++ src/user/sms/gupshup/gupshup.service.ts
@@ -13,8 +13,6 @@ import {
 import { Injectable } from '@nestjs/common';
 import { SmsService } from '../sms.service';
 import got from 'got/dist/source';
-import { parse } from 'path/posix';
-import { response } from 'express';
 
 @Injectable()
 export class GupshupService extends SmsService implements SMS {
@@ -85,7 +83,7 @@ export class GupshupService extends SmsService implements SMS {
       .get(url, options)
       .then((response): OTPResponse => {
         status.networkResponseCode = 200;
-        const r = parseResponse(response.body);
+        const r = this.parseResponse(response.body);
         status.messageID = r.messageID;
         status.error = r.error;
         status.providerResponseCode = r.providerResponseCode;
@@ -108,45 +106,51 @@ export class GupshupService extends SmsService implements SMS {
       });
   }
 
+  verifyOTP(data: SMSData): Promise<OTPResponse> {
+    
+    return null;
+  }
+
   getOTPTemplate() {
     return process.env.GUPSHUP_OTP_TEMPLATE;
   }
-}
-function parseResponse(response: string) {
-  const responseData: string[] = response.split('|').map((s) => s.trim());
-  try {
-    if (responseData[0] === 'success') {
+
+  parseResponse(response: string) {
+    const responseData: string[] = response.split('|').map((s) => s.trim());
+    try {
+      if (responseData[0] === 'success') {
+        return {
+          providerResponseCode: null,
+          status: SMSResponseStatus.success,
+          messageID: responseData[2],
+          error: null,
+          providerSuccessResponse: responseData[3],
+        };
+      } else {
+        const error: SMSError = {
+          errorText: responseData[2],
+          errorCode: responseData[1],
+        };
+        return {
+          providerResponseCode: responseData[1],
+          status: SMSResponseStatus.failure,
+          messageID: null,
+          error,
+          providerSuccessResponse: null,
+        };
+      }
+    } catch (e) {
+      const error: SMSError = {
+        errorText: `Gupshup response could not be parsed :: ${e.message}`,
+        errorCode: 'CUSTOM ERROR',
+      };
       return {
         providerResponseCode: null,
-        status: SMSResponseStatus.success,
-        messageID: responseData[2],
-        error: null,
-        providerSuccessResponse: responseData[3],
-      };
-    } else {
-      const error: SMSError = {
-        errorText: responseData[2],
-        errorCode: responseData[1],
-      };
-      return {
-        providerResponseCode: responseData[1],
         status: SMSResponseStatus.failure,
         messageID: null,
         error,
         providerSuccessResponse: null,
       };
     }
-  } catch (e) {
-    const error: SMSError = {
-      errorText: `Gupshup response could not be parsed :: ${e.message}`,
-      errorCode: 'CUSTOM ERROR',
-    };
-    return {
-      providerResponseCode: null,
-      status: SMSResponseStatus.failure,
-      messageID: null,
-      error,
-      providerSuccessResponse: null,
-    };
   }
 }
