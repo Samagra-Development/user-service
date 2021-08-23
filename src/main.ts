694267929dedab503107d6076599b703673401c8
ChakshuGautam <chaks.gautam@gmail.com>
2021-08-23 15:12:02 +0530

    feat(add swagger): swagger Setup (API docs currently don't include details)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('e-Samwad User Service')
+    .setDescription('User Service APIs')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
