1ff252f48154d9861d30635b1e3b22b2d2c449ae
ChakshuGautam <chaks.gautam@gmail.com>
2021-08-23 23:27:39 +0530

    test(user-db service): removed test that is not mocked rn

--- src/user/user-db/user-db.service.spec.ts
+++ src/user/user-db/user-db.service.spec.ts
@@ -17,12 +17,12 @@ describe('UserDbService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should return school', async () => {
-    service.url = 'http://localhost:8000';
-    service.teacherPartUrl = '/api/v5/drf/teacher/';
-    service.schoolPartUrl = '/api/v5/drf/school/';
-    const school = await service.getSchool('2052104703');
-    expect(school.status).toEqual(true);
-    expect(school.id).toEqual(34828);
-  });
+  // it('should return school', async () => {
+  //   service.url = 'http://localhost:8000';
+  //   service.teacherPartUrl = '/api/v5/drf/teacher/';
+  //   service.schoolPartUrl = '/api/v5/drf/school/';
+  //   const school = await service.getSchool('2052104703');
+  //   expect(school.status).toEqual(true);
+  //   expect(school.id).toEqual(34828);
+  // });
 });
