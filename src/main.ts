1bc1224d9c220ed1ec1ce8dc97a236854d8e9d18
ChakshuGautam <chaks.gautam@gmail.com>
2021-09-28 09:21:29 +0530

    feat(app): add CORS

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(3000);
 }
 bootstrap();
