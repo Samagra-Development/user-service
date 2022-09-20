5188562b1e6854e5a38183ac463853cfb1977de6
Abhishek Mishra <abhishek@luezoid.com>
2022-09-20 16:17:52 +0530

    User/Login: added support to discard encryption based on environment variable

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,10 @@
-import { SearchResponse } from '@fusionauth/typescript-client';
-import ClientResponse from '@fusionauth/typescript-client/build/src/ClientResponse';
 import { Body, Controller, Get, Patch, Post, Query } from '@nestjs/common';
-import { SkipThrottle } from '@nestjs/throttler';
 import { ChangePasswordDTO } from './dto/changePassword.dto';
 
 import { FusionauthService } from './fusionauth/fusionauth.service';
 import { OtpService } from './otp/otp.service';
 import { SMSResponse } from './sms/sms.interface';
-import { SignupResponse, UsersResponse } from './user.interface';
+import { SignupResponse } from './user.interface';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -17,7 +14,7 @@ export class UserController {
     private readonly otpService: OtpService,
     private readonly userService: UserService,
   ) {}
-  
+
   @Get('/verify')
   async verifyUsernamePhoneCombination(): Promise<any> {
     const status: boolean =
@@ -39,40 +36,35 @@ export class UserController {
 
   @Post('/signup')
   async signup(@Body() user: any): Promise<SignupResponse> {
-    const status: SignupResponse = await this.userService.signup(user);
-    return status;
+    return await this.userService.signup(user);
   }
 
   @Post('/login')
   async login(@Body() user: any): Promise<SignupResponse> {
-    if(user.applicationId === process.env.FUSIONAUTH_APPLICATION_ID){
-      user.loginId = this.userService.encrypt(user.loginId)
-      user.password = this.userService.encrypt(user.password)
+    if (
+      process.env.ENCRYPTION_ENABLED === 'TRUE' &&
+      user.applicationId === process.env.FUSIONAUTH_APPLICATION_ID
+    ) {
+      user.loginId = this.userService.encrypt(user.loginId);
+      user.password = this.userService.encrypt(user.password);
     }
-    const status: SignupResponse = await this.userService.login(user);
-    return status;
+    return await this.userService.login(user);
   }
 
   @Patch('/update')
   async update(@Body() user: any): Promise<SignupResponse> {
-    const status: SignupResponse = await this.userService.update(user);
-    return status;
+    return await this.userService.update(user);
   }
 
   @Post('/changePassword/sendOTP')
   async changePasswordOTP(@Body() data: any): Promise<SignupResponse> {
-    const status: SignupResponse = await this.userService.changePasswordOTP(
-      data.username,
-    );
-    return status;
+    return await this.userService.changePasswordOTP(data.username);
   }
 
   @Patch('/changePassword/update')
   async changePassword(
     @Body() data: ChangePasswordDTO,
   ): Promise<SignupResponse> {
-    const status: SignupResponse = await this.userService.changePassword(data);
-    return status;
+    return await this.userService.changePassword(data);
   }
-
 }
