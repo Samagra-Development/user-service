7a71db3e36c6c27d54a7af4a7e0326cbbaf975e2
Radhay <radhay@samagragovernance.in>
2022-09-25 02:12:16 +0530

    Fix: application id not resolving in docker-compose

--- src/api/config.resolver.service.ts
+++ src/api/config.resolver.service.ts
@@ -7,26 +7,35 @@ export class ConfigResolverService {
     constructor(private configService: ConfigService) {
     }
 
+    transform(applicationId: string): string {
+        return applicationId.split("_").join("-");
+    }
+
     getConfigByApplicationId(applicationId: string): ApiConfig {
+        applicationId = this.transform(applicationId);
         return this.configService.get<ApiConfig>(applicationId);
     }
 
     getHost(applicationId: string): string {
+        applicationId = this.transform(applicationId);
         const config = this.configService.get<string>(applicationId);
         return JSON.parse(config).host;
     }
 
     getApiKey(applicationId: string): string {
+        applicationId = this.transform(applicationId);
         const config = this.configService.get<string>(applicationId);
         return JSON.parse(config).apiKey || null;
     }
 
     getEncryptionStatus(applicationId: string): boolean {
+        applicationId = this.transform(applicationId);
         const config = this.configService.get<string>(applicationId);
         return JSON.parse(config).encryption.enabled || false;
     }
 
     getEncryptionKey(applicationId: string): string{
+        applicationId = this.transform(applicationId);
         const config = this.configService.get<string>(applicationId);
         return JSON.parse(config).encryption.key || null;
     }
