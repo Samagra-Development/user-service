47ff09243c38331ee3d5d9e4c200957db1e9a2ff
Abhishek Mishra <abhishek@luezoid.com>
2023-07-26 11:53:48 +0530

    SecurityHeaders: added package helmet for security headers[default setting]

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import * as Sentry from '@sentry/node';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,10 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
+  // add security headers
+  app.use(helmet());
+
+  // enable cors
   app.enableCors({
     origin: process.env.CORS_ALLOWED_ORIGINS?.split(/\s*,\s*/) ?? '*',
     credentials: true,
