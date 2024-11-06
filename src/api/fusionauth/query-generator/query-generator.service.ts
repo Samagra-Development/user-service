fcf685976e8ae6a08cdd0403c776a21344b7135f
KDwevedi <kanav11dwevedi@gmail.com>
2024-11-06 15:13:26 +0530

    updated query builder

--- src/api/fusionauth/query-generator/query-generator.service.ts
+++ src/api/fusionauth/query-generator/query-generator.service.ts
@@ -2,78 +2,77 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class QueryGeneratorService {
-    constructor(){}
+  constructor() {}
 
-    queryUsersByApplicationId(applicationId: string): string{
-        const query = {
+  queryUsersByApplicationId(applicationId: string): string {
+    const query = {
+      bool: {
+        must: [
+          {
+            nested: {
+              path: 'registrations',
+              query: {
+                bool: {
+                  must: [
+                    {
+                      match: {
+                        'registrations.applicationId': applicationId,
+                      },
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        ],
+      },
+    };
+    return JSON.stringify(query);
+  }
+
+  queryUsersByApplicationIdAndQueryString(
+    applicationId: string[],
+    queryString: string,
+  ): string {
+    const query = {
+      bool: {
+        must: [
+          {
             bool: {
               must: [
                 {
                   nested: {
-                    path: "registrations",
+                    path: 'registrations',
                     query: {
                       bool: {
-                        must: [
-                          {
-                            match: {
-                              "registrations.applicationId": applicationId
-                            }
-                          }
-                        ]
-                      }
-                    }
-                  }
-                }
-              ]
-            }
-          }
-          return JSON.stringify(query);
-    }
-
-    queryUsersByApplicationIdAndQueryString(applicationId: string[], queryString: string): string{
-        const query = {
-            bool: {
-              must: [
-                {
-                  bool: {
-                    must: [
-                      [
-                        {
-                          nested: {
-                            path: "registrations",
-                            query: {
-                              bool: {
-                                should: this.createMatchTags(applicationId)
-                              }
-                            }
-                          }
-                        }
-                      ]
-                    ]
-                  }
+                        should: this.createMatchTags(applicationId),
+                      },
+                    },
+                  },
                 },
-                {
-                  query_string: {
-                    query: queryString
-                  }
-                }
-              ]
-            }
-          }
-          return JSON.stringify(query)
-    }
+              ],
+            },
+          },
+          {
+            query_string: {
+              query: queryString,
+            },
+          },
+        ],
+      },
+    };
+    return JSON.stringify(query);
+  }
 
-    createMatchTags(arr: string[]): Array<{match: any}>{
-        let tags:  Array<{match: any}> = [];
-        for(let x of arr){
-            tags.push(
-                {
-                    match: {
-                      "registrations.applicationId": x
-                    }
-                }
-            )
-        }
-        return tags;
+  createMatchTags(arr: string[]): Array<{ match: any }> {
+    const tags: Array<{ match: any }> = [];
+    for (const x of arr) {
+      tags.push({
+        match: {
+          'registrations.applicationId': x,
+        },
+      });
     }
+    return tags;
+  }
 }
